0ed3ab6b245bb76f746baa79d46807302416b7a2
Elston Aw <elston.ayx@u.nus.edu>
2018-10-02 14:03:23 +0800

    A2.2 Added RemarkCommand Class Modified AddressBookParser to accept RemarkCommand with command 'remark'

---
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -0,0 +1,13 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+public class RemarkCommand extends Command {
+    public static final String COMMAND_WORD = "remark";
+
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+        throw new CommandException("New Remark Exception");
+    };
+}
