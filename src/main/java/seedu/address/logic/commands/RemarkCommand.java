394f1a0c5f06eda2bb62bea5bda23ac83ed36721
Elston Aw <elston.ayx@u.nus.edu>
2018-10-15 19:39:39 +0800

    A2.2 [Step 1] Modified RemarkCommand to throw MESSAGE_NOT_IMPLEMENTED_YET Created new RemarkCommandTest to assertCommandFailure on executing of RemarkCommand Modified AddressBookParserTest to assertTrue AddressBook parseCommand of RemarkCommand is an instance of RemarkCommand

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -6,8 +6,9 @@ import seedu.address.model.Model;
 
 public class RemarkCommand extends Command {
     public static final String COMMAND_WORD = "remark";
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Remark command not implemented yet";
 
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
-        throw new CommandException("New Remark Exception");
+        throw new CommandException(MESSAGE_NOT_IMPLEMENTED_YET);
     };
 }
--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -25,6 +25,7 @@ import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.HistoryCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.RedoCommand;
+import seedu.address.logic.commands.RemarkCommand;
 import seedu.address.logic.commands.SelectCommand;
 import seedu.address.logic.commands.UndoCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -167,5 +168,10 @@ public class AddressBookParserTest {
         thrown.expect(ParseException.class);
         thrown.expectMessage(MESSAGE_UNKNOWN_COMMAND);
         parser.parseCommand("unknownCommand");
+    }
+
+    @Test
+    public void parseCommand_remark() throws Exception {
+        assertTrue(parser.parseCommand(RemarkCommand.COMMAND_WORD) instanceof RemarkCommand);
     }
 }
